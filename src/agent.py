ccdd1a3c9a5a8222e10c5b34ed81c8e035d42047
rodrigo-tripa <rodrigojesustripa@proton.me>
2026-09-20 12:04:30 +0100

    Refactor agent to use Genome

--- src/agent.py
+++ src/agent.py
@@ -1,34 +1,40 @@
 import random
 
+class Genome:
+    def __init__(self, alelos):
+        self.alelos = alelos
+
 class Agent: 
-    def __init__(self, alelo1, alelo2):
-        self.alelo1 = alelo1
-        self.alelo2 = alelo2
+    def __init__(self, genome):
+        self.genome = genome
 
     def get_size(self):
-        if self.alelo1 == "A" and self.alelo2 == "A":
+        if self.genome.alelos[0] == "A" and self.genome.alelos[1] == "A":
             size = 80
-        elif self.alelo1 == "A" or self.alelo2 == "A":
+        elif self.genome.alelos[0] == "A" or self.genome.alelos[1] == "A":
             size = 65
-        elif self.alelo1 == "a" and self.alelo2 == "a":
+        elif self.genome.alelos[0] == "a" and self.genome.alelos[1] == "a":
             size = 50
 
         return size
     
     def reproduce(self, partner):
 
-        self_alelos = [self.alelo1, self.alelo2]
-        self_alelo = random.choice(self_alelos)
+        self_alelo = random.choice(self.genome.alelos)
 
-        partner_alelos = [partner.alelo1, partner.alelo2]
-        partner_alelo = random.choice(partner_alelos)
+        partner_alelo = random.choice(partner.genome.alelos)
 
-        new_agent = Agent(self_alelo, partner_alelo)
+        new_genome = Genome([self_alelo, partner_alelo])
+
+        new_agent = Agent(new_genome)
 
         return new_agent
 
-agent1 = Agent("A", "a")
-agent2 = Agent("A", "a")
+genome1 = Genome(["A", "a"])
+agent1 = Agent(genome1)
+
+genome2 = Genome(["A", "a"])
+agent2 = Agent(genome2)
 
 print(agent1.get_size())
 print(agent2.get_size())
